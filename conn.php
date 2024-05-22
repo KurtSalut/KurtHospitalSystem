10da6548c305a4bccc61d7eb3a2af8bf032a3873
KurtSalut <kadencemalik29@gmail.com>
2024-05-22 21:14:11 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,27 @@
+<?php
+
+class conn{
+	private $hostdb = "localhost";
+	private $userdb = "root";
+	private $passdb = "";
+	private $namedb = "hospital_management_system";
+	public $conn;
+	public $pdo;
+
+	public function __construct(){
+		if (!isset($this->pdo)){
+			try {
+				$link = new PDO("mysql:host=".$this->hostdb.";dbname=".$this->namedb, $this->userdb, $this->passdb);
+				$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+				$link->exec("SET CHARACTER SET utf8");
+				$this->conn = $link;
+ 
+			} catch (PDOException $e){
+				die("Failed to connect with Database".$e->getMessage());	
+			}
+		}
+		
+	}
+
+}
+?> 
